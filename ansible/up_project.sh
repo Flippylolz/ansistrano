8dccc25fc19f05dca5e12cb41e1308c18edf07ed
Dmytro Leschenko <dmytro.leschenko@masterofcode.com>
2019-04-22 21:12:54 +0300

    deploy

--- ansible/up_project.sh
+++ ansible/up_project.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-docker-compose -f docker-compose.poduction.yml build
-docker-compose -f docker-compose.poduction.yml run --rm rails rake db:create
-docker-compose -f docker-compose.poduction.yml run --rm rails rake db:migrate
-docker-compose -f docker-compose.poduction.yml stop
-docker-compose -f docker-compose.poduction.yml up -d
+docker-compose -f docker-compose.production.yml build
+docker-compose -f docker-compose.production.yml run --rm rails rake db:create
+docker-compose -f docker-compose.production.yml run --rm rails rake db:migrate
+docker-compose -f docker-compose.production.yml stop
+docker-compose -f docker-compose.production.yml up -d
 sleep 5
 docker ps
 
